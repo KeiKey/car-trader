85c96fb3df58332c2f8f1f8c7f08e822e5de24bb
kei selami <kei.selami@eurostep.it>
2022-09-05 15:13:21 +0200

    Fill the vehicle form with vehicle data

--- resources/views/partials/forms/vehicle-form.blade.php
+++ resources/views/partials/forms/vehicle-form.blade.php
@@ -1,12 +1,15 @@
-<form method="POST" action="{{ route('vehicles.store') }}" id="vehicle-form">
+<form action="{{ isset($vehicle) ? route('vehicles.update', ['vehicle' => $vehicle]) : route('vehicles.store') }}" method="POST">
     @csrf
+    @isset($vehicle)
+        @method('PUT')
+    @endisset
 
     <div class="row mb-3">
         <label for="serial_number" class="col-md-3 col-form-label text-md-end">{{ __('general.serial_number') }} (17)</label>
 
         <div class="col-md-9">
             <input id="serial_number" type="text" class="form-control @error('serial_number') is-invalid @enderror"
-                   name="serial_number" value="{{ old('serial_number') }}" required minlength="17" maxlength="17">
+                   name="serial_number" value="{{ old('serial_number', $vehicle->serial_number ?? '') }}" required minlength="17" maxlength="17">
 
             @error('serial_number')
             <span class="invalid-feedback" role="alert">
@@ -20,7 +23,8 @@
         <label for="make" class="col-md-3 col-form-label text-md-end">{{ __('general.make') }}</label>
 
         <div class="col-md-9">
-            <input id="make" type="text" class="form-control @error('make') is-invalid @enderror" name="make" value="{{ old('make') }}" required>
+            <input id="make" type="text" class="form-control @error('make') is-invalid @enderror" name="make"
+                   value="{{ old('make', $vehicle->make ?? '') }}" required>
 
             @error('make')
             <span class="invalid-feedback" role="alert">
@@ -34,7 +38,8 @@
         <label for="model" class="col-md-3 col-form-label text-md-end">{{ __('general.model') }}</label>
 
         <div class="col-md-9">
-            <input id="model" type="text" class="form-control @error('model') is-invalid @enderror" name="model" value="{{ old('model') }}" required>
+            <input id="model" type="text" class="form-control @error('model') is-invalid @enderror" name="model"
+                   value="{{ old('model', $vehicle->model ?? '') }}" required>
 
             @error('model')
             <span class="invalid-feedback" role="alert">
@@ -53,7 +58,8 @@
                     <span class="input-group-text rounded-0 rounded-start">Cc</span>
                 </div>
 
-                <input id="engine_size" type="number" min="250" max="7000" value="1390" class="form-control @error('engine_size') is-invalid @enderror" name="engine_size" required step="1">
+                <input id="engine_size" type="number" min="250" max="7000" value="{{ old('engine_size', $vehicle->engine_size ?? '1390') }}"
+                       class="form-control @error('engine_size') is-invalid @enderror" name="engine_size" required step="1">
             </div>
 
             @error('engine_size')
@@ -68,7 +74,8 @@
         <label for="color" class="col-md-3 col-form-label text-md-end">{{ __('general.color') }}</label>
 
         <div class="col-md-9">
-            <input id="color" type="text" class="form-control @error('color') is-invalid @enderror" name="color" value="{{ old('color') }}" required>
+            <input id="color" type="text" class="form-control @error('color') is-invalid @enderror" name="color"
+                   value="{{ old('color', $vehicle->color ?? '') }}" required>
 
             @error('color')
             <span class="invalid-feedback" role="alert">
@@ -82,7 +89,8 @@
         <label for="production_year" class="col-md-3 col-form-label text-md-end">{{ __('general.production_year') }}</label>
 
         <div class="col-md-9">
-            <input id="production_year" type="number" min="1900" value="2000" max="2025" class="form-control @error('production_year') is-invalid @enderror" name="production_year" required step="1">
+            <input id="production_year" type="number" min="1900" max="2025" value="{{ old('production_year', $vehicle->production_year ?? '2000') }}"
+                   class="form-control @error('production_year') is-invalid @enderror" name="production_year" required step="1">
 
             @error('production_year')
             <span class="invalid-feedback" role="alert">
@@ -101,7 +109,8 @@
                     <span class="input-group-text rounded-0 rounded-start">$</span>
                 </div>
 
-                <input id="price" type="number" min="0" value="1.00" class="form-control @error('price') is-invalid @enderror" name="price" required step="0.01">
+                <input id="price" type="number" min="0" value="{{ old('price', $vehicle->price ?? '1.00') }}"
+                       class="form-control @error('price') is-invalid @enderror" name="price" required step="0.01">
             </div>
 
             @error('price')
@@ -117,7 +126,8 @@
 
         <div class="col-md-9">
             <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" role="switch" id="active" name="active" value="1">
+                <input class="form-check-input" @isset($vehicle) @if(!$vehicle->deactivated_at) checked @endif @endisset
+                type="checkbox" role="switch" id="active" name="active" value="1">
             </div>
         </div>
     </div>
