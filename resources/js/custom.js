8994779c0cb6af44a74f700831026c7d6e1fc3b3
kei selami <kei.selami@eurostep.it>
2022-09-05 17:29:59 +0200

    Comment out category script for vahicles

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,11 +1,11 @@
-let stationForm = document.getElementById('vehicle-form');
-
-stationForm.addEventListener('submit', function (event) {
-    $('#category-table tbody tr').each(function() {
-        $('<input>', {
-            type: 'hidden',
-            name: 'vehicleCategories[]',
-            value: $(this).find(`input.vehicle-category-extra`).val()+`~`+$(this).find(`select.vehicle-category`).val()
-        }).appendTo(stationForm);
-    })
-})
+// let stationForm = document.getElementById('vehicle-form');
+//
+// stationForm.addEventListener('submit', function (event) {
+//     $('#category-table tbody tr').each(function() {
+//         $('<input>', {
+//             type: 'hidden',
+//             name: 'vehicleCategories[]',
+//             value: $(this).find(`input.vehicle-category-extra`).val()+`~`+$(this).find(`select.vehicle-category`).val()
+//         }).appendTo(stationForm);
+//     })
+// })
