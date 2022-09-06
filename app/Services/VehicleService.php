e70aff887c804c8c9c8074da066b537441fdef5a
kei selami <kei.selami@eurostep.it>
2022-09-06 04:00:07 +0200

    Improve Create vehicle functionality

--- app/Services/VehicleService.php
+++ app/Services/VehicleService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Category\Category;
 use App\Models\Vehicle\Vehicle;
 use Exception;
 use Illuminate\Support\Facades\DB;
@@ -30,10 +31,7 @@ class VehicleService
                 'disabled_at'     => $vehicleData['active'] ? now() : null
             ]);
 
-            foreach ($vehicleData['vehicleCategories'] as $vehicleCategory) {
-                $data = explode('~', $vehicleCategory);
-                $vehicle->categories()->attach($data[1], ['extra' => $data[0]]);
-            }
+            $this->attachCategoriesToVehicle($vehicle, $vehicleData['vehicleCategories']);
 
             return $vehicle;
         });
@@ -61,10 +59,10 @@ class VehicleService
                 'disabled_at'     => $vehicleData['active'] ? now() : null
             ]);
 
-            foreach ($vehicleData['vehicleCategories'] as $vehicleCategory) {
-                $data = explode('~', $vehicleCategory);
-                $vehicle->categories()->attach($data[1], ['extra' => $data[0]]);
-            }
+//            foreach ($vehicleData['vehicleCategories'] as $vehicleCategory) {
+//                $data = explode('~', $vehicleCategory);
+//                $vehicle->categories()->attach($data[1], ['extra' => $data[0]]);
+//            }
 
             return $vehicle;
         });
@@ -80,5 +78,31 @@ class VehicleService
     public function deleteVehicle(Vehicle $vehicle): void
     {
         $vehicle->delete();
+    }
+
+    /**
+     * Attach categories to vehicle.
+     *
+     * @param Vehicle $vehicle
+     * @param $vehicleCategories
+     * @return void
+     */
+    private function attachCategoriesToVehicle(Vehicle $vehicle, $vehicleCategories): void
+    {
+        $attachedCategories = [];
+
+        foreach ($vehicleCategories as $vehicleCategory) {
+            $data = explode('~', $vehicleCategory);
+            $categoryId = $data[1];
+            $extra = $data[0];
+
+            $category = Category::query()->firstWhere('id', $categoryId);
+            if (!$category || in_array($categoryId, $attachedCategories)) {
+                continue;
+            }
+
+            $vehicle->categories()->attach($categoryId, ['extra' => $extra]);
+            $attachedCategories[] = $categoryId;
+        }
     }
 }
